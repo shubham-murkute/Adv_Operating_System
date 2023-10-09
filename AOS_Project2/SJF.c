8ef574984de7fbae491dfefa815bf44e79a6a678
Kyle Mondina <kylemondinaportfolio@gmail.com>
2023-10-08 21:30:25 -0700

    sjf implementation

---
+++ AOS_Project2/SJF.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stat.h"
+#include "utility.h"
+
+process_stat *createProcessStat2(process *proc) {
+	process_stat *ps = (process_stat *) malloc(sizeof(process_stat));
+	ps->proc = proc;
+	ps->waitingTime = 0;
+	ps->turnaroundTime = 0;
+	ps->responseTime = 0;
+
+	ps->runTime = 0;
+	ps->startTime = -1;
+	ps->endTime = -1;
+	return ps;
+}
+
+
+
+int compareRunTimes(void *data1, void *data2) {
+	process *p1 = (process *)data1;
+	process *p2 = (process *)data2;
+	if(p1->runTime < p2->runTime) {
+		return -1;
+	} else if(p1->runTime == p2->runTime){
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+
+
+average_stats shortestJobFirstNP(linked_list *processes) {
+	int t = 0;
+
+	//Creation of Process Queue
+	queue *processQueue = (queue *)createQueue();
+	node * procPtr = processes->head;
+	if(processes->head == NULL) {
+		fprintf(stderr," There is no Process to schedule\n\n");
+	}
+	
+	//keep checking while time quanta is less than 100 or the process queue is empty...
+	process_stat * scheduledProcess = NULL;
+
+	linked_list *ll = createLinkedList();
+	printf("\nShortest Job First Algorithm:\n");
+	while(t<100 || scheduledProcess!=NULL) {
+		//check for incoming new process and do enqueue.
+		if(procPtr != NULL) {
+			process * newProcess = (process *)(procPtr->data);
+			if(newProcess->arrivalTime <= t) {
+				enqueue(processQueue,createProcessStat2(newProcess));
+				procPtr = procPtr->next;
+			}
+		}
+
+        // Sort the processQueue by runTime (shortest runTime first)
+
+		//check process queue and schedule it if there is no scheduled process now.
+		if(scheduledProcess == NULL && processQueue->size > 0) {
+            sort(processQueue, compareRunTimes);
+			scheduledProcess = (process_stat *) dequeue(processQueue);
+		}
+
+        
+		if(scheduledProcess != NULL) {
+			process * proc = scheduledProcess->proc;
+
+			//add  the currently running process to the time chart
+			printf("%c",proc->pid);
+
+			//update current processes stat
+			if(scheduledProcess->startTime == -1) {
+				scheduledProcess->startTime = t;
+			}
+			scheduledProcess->runTime++;
+    //check if scheduled process run time is greater than the currently running process runtime..
+			if(scheduledProcess->runTime >= proc->runTime) {
+				scheduledProcess->endTime = t;
+				//add the scheduled process.
+				addNode(ll,scheduledProcess);
+				scheduledProcess = NULL;
+			}
+		} else {
+			printf("_");
+		}
+		//keep increasing the time.
+		t++;
+	}
+
+	//Print the Stat of the process.
+	return printPolicyStat(ll);
+	
+}
