dad085f299c67fd2063758cf48daf88e6d00e226
KyleMondinaPortfolio <kylemondinaportfolio@gmail.com>
2023-10-14 17:50:27 -0700

    feat: define concertSeats object

---
+++ AOS_Project3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "utility.h"
+
+typedef struct {
+    /*
+    the concert seating chart as a 10-by-10 matrix that 
+    shows which customer was assigned to each seat. Identify ticket seller H’s customers as H001, H002, H003, ...; the customers of ticket sellers M1, M2, …, as 
+    M101, M102, …, M201, M202, …; and the customers of ticket sellers L1, L2, … 
+    as L101, L102, …, L201, L202, ... You can indicate still-unsold seats with dashes
+    */
+    char seatChart[10][10][5];
+    int availableSeats;
+} Seats
+
+int main (int argc, char **argv){
+
+    Seats concertSeats;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            strcpy(concertSeats.seatChart[i][j], "----");
+        }
+    }
+    concertSeats.availableSeats = 100;
+    
+
+}
