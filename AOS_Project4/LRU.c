fb096dab7cde7625b22a6306fd2d01692e556101
Kyle Mondina <kylemondinaportfolio@gmail.com>
2023-10-24 23:00:39 -0700

    revised lru

--- AOS_Project4/LRU.c
+++ AOS_Project4/LRU.c
@@ -1,6 +1,7 @@
 #include "PAGE.h"
 
 // LRU algorithm
+/*
 void LRU_FUNCTION(LISTOFPAGES* plLRU) {
     page* current = plLRU->HDL;
     page* leastRecent = NULL;
@@ -19,4 +20,23 @@ void LRU_FUNCTION(LISTOFPAGES* plLRU) {
         leastRecent->pid = -1;
         leastRecent->PGENUMBER = -1;
     }
+}
+*/
+
+
+//LRU algorithm
+void LRU_FUNCTION(LISTOFPAGES* plLRU) {
+    page* counter = plLRU->HDL;
+    page* evicted = plLRU->HDL;
+    int lru = counter->LONE;
+    while(counter) {
+        if(counter->LONE < lru){
+            evicted = counter;
+            lru = counter->LONE;
+        }
+        counter = counter->next;
+    }
+    if (DEBUG==1) printf("EVICTED ones :: p[%03d] c:%02d l:%02f\n", evicted->pid, evicted->CNTER, evicted->LONE);
+    evicted->pid = -1;
+    evicted->PGENUMBER = -1;
 }
